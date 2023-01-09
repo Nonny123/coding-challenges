a67cc4d9921d20b0768f19520bc9aa490ee4f5dc
Nonso <justcoolk@yahoo.com>
2023-01-09 21:30:44 +0100

    create test class for Anagrams

---
+++ coding-challenge/coding-challenge-tests/StringsTests/AnagramsTests/AnagramsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace coding_challenge_tests.StringsTests.AnagramsTests
+{
+    public class AnagramsTests
+    {
+        [Fact]
+        public void IsAnagrams()
+        {
+            
+
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
