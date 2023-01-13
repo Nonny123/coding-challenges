3c2abf51df2aebe2f6e72569b8f4e5f8875957b9
Nonso <justcoolk@yahoo.com>
2023-01-13 21:15:09 +0100

    anagram hackerrank

--- coding-challenge/coding-challenge/Strings/Anagrams/Anagrams.cs
+++ coding-challenge/coding-challenge/Strings/Anagrams/Anagrams.cs
@@ -8,5 +8,46 @@ namespace coding_challenge.Strings.Anagrams
 {
     public static class Anagrams
     {
-    }
+
+        public void CheckAnagram()
+        {
+            for (int i = 0; i < T; i++)
+            {
+
+                if (!IsEven(s.Length))
+                {
+                    Console.WriteLine(-1);
+                }
+                else
+                {
+                    string a = s.Substring(0, s.Length / 2);
+                    string b = s.Substring(s.Length / 2, s.Length / 2);
+
+                    Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+                    for (int j = 0; j < a.Length; j++)
+                    {
+                        if (!dictionary.ContainsKey(a[j]))
+                            dictionary.Add(a[j], 0);
+
+                        dictionary[a[j]]++;
+                    }
+
+                    for (int j = 0; j < b.Length; j++)
+                    {
+                        if (!dictionary.ContainsKey(b[j]))
+                            dictionary.Add(b[j], 0);
+
+                        dictionary[b[j]]--;
+                    }
+
+                    int sum = 0;
+
+                    foreach (KeyValuePair<char, int> keyValuePair in dictionary)
+                    {
+                        sum += Math.Abs(keyValuePair.Value);
+                    }
+                }
+            }
+   
 }
