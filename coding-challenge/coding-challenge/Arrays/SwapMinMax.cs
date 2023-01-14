9fc8d3298216ff180a98206cc6beb2575e682c64
Nonso <justcoolk@yahoo.com>
2023-01-14 22:08:51 +0100

    Get swap min max in array

---
+++ coding-challenge/coding-challenge/Arrays/SwapMinMax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge.Arrays
+{
+    public static class SwapMinMax
+    {
+        public static void MinMaxSwap(int[] x)
+        {
+            int min = 0;
+            int max = 0;
+
+            for (int i = 1; i < x.Length; i++)
+            {
+                if (x[min] > x[i])
+                    min = i;
+                if (x[max] < x[i])
+                    max = i;
+            }
+
+            int temp = x[min];
+
+            x[min] = x[max];
+            x[max] = temp;
+
+            Console.WriteLine(x[min]);
+            Console.WriteLine(x[max]);
+
+        }
+    }
+}
