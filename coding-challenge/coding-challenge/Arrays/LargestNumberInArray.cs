7e2f352a716877fd3b932a38eeaa437d5a68baa1
Nonso <justcoolk@yahoo.com>
2023-01-15 21:41:00 +0100

    Get largest number in array

---
+++ coding-challenge/coding-challenge/Arrays/LargestNumberInArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge.Arrays
+{
+   
+    public static class LargestNumberInArray
+    {
+        //sample  input -> int[]arr ={10,34,47,80, 6785};
+        public static int GetLargestNumberInArray(int[] arr)
+        {
+          
+            int max = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+
+            return max;
+
+        }
+    }
+}
