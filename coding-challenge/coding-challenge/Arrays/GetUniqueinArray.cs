59fb5ddededfcff68df9410ae5201c581a7231a3
Nonso <justcoolk@yahoo.com>
2023-01-17 21:45:49 +0100

    unique in array

---
+++ coding-challenge/coding-challenge/Arrays/GetUniqueinArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge.Arrays
+{
+    class 
+    {
+    }
+    public static class GetUniqueinArray
+    {
+
+        public static int UniqueinArray(int[] arr)
+        {
+
+            int i,j;
+            bool unique;
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                for (j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                        unique = false;
+                }
+            }
+
+            //return ;
+
+        }
+    }
+}
