368e8ff180dd29bb2fd2795e04429ab80623745b
Nonso <justcoolk@yahoo.com>
2023-01-20 23:10:58 +0100

    initial two sum array

---
+++ coding-challenge/coding-challenge/Arrays/TwoSumArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge.Arrays
+{
+    public static class TwoSumArray
+    {
+        public static List<int> TwoIntegersSumTarget(int[] arr, int target)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                       
+                        break;
+                    }
+                }
+               
+            }
+
+            return false;
+
+        }
+    }
+}
