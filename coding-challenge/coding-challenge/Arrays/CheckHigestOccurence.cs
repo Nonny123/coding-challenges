d4912891658f68a99752566fc943fa02e1d8bf53
Nonso <justcoolk@yahoo.com>
2023-01-27 23:12:15 +0100

    most frequent numbers

--- coding-challenge/coding-challenge/Arrays/CheckHigestOccurence.cs
+++ coding-challenge/coding-challenge/Arrays/CheckHigestOccurence.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace coding_challenge.Arrays
 {
-    class CheckHigestOccurence
+    public class CheckHigestOccurence
     {
+        public static bool MostFrequentNumbers(int[] x)
+        {
+
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            foreach (int i in x)
+            {
+                if (d.ContainsKey(i))
+                    return true;
+                else d.Add(i, 1);
+            }
+
+            return false;
+
+        }
     }
 }
