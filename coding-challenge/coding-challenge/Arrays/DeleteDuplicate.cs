03373c2480071f420fabfd1734781a3ee1ae4d61
Nonso <justcoolk@yahoo.com>
2023-02-01 22:53:45 +0100

    implement delete duplicate

--- coding-challenge/coding-challenge/Arrays/DeleteDuplicate.cs
+++ coding-challenge/coding-challenge/Arrays/DeleteDuplicate.cs
@@ -8,5 +8,32 @@ namespace coding_challenge.Arrays
 {
     class DeleteDuplicate
     {
+        public static int[] DeleteDuplicateInArray(int[] arr)
+        {
+
+            int i, j;
+            bool unique;
+            List<int> uniqueElements = new List<int>();
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                unique = true;
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        unique = false;
+                        break;
+                    }
+                }
+                if (unique)
+                {
+                    uniqueElements.Add(arr[i]);
+                }
+            }
+
+            return uniqueElements;
+
+        }
     }
 }
