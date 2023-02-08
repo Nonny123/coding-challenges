95a4db9a7fab67932fb48aedce419ff508a480b6
Nonso <justcoolk@yahoo.com>
2023-02-08 23:17:22 +0100

    insert element in array

--- coding-challenge/coding-challenge/Arrays/InsertElement.cs
+++ coding-challenge/coding-challenge/Arrays/InsertElement.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace coding_challenge.Arrays
 {
-    class InsertElement
+    public class InsertElement
     {
+        public static List<int> InsertElementInErray(int[] arr)
+        {
+
+            int i, j;
+            bool unique;
+            List<int> uniqueElements = new List<int>();
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                unique = true;
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        unique = false;
+                        break;
+                    }
+                }
+                if (unique)
+                {
+                    uniqueElements.Add(arr[i]);
+                }
+            }
+
+            return uniqueElements;
+
+        }
     }
 }
