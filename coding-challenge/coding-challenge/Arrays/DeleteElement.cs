3f21aced693e82cddd12c8bebd6ad921e537b276
Nonso <justcoolk@yahoo.com>
2023-02-15 21:56:29 +0100

    implement delete element in array

--- coding-challenge/coding-challenge/Arrays/DeleteElement.cs
+++ coding-challenge/coding-challenge/Arrays/DeleteElement.cs
@@ -8,5 +8,54 @@ namespace coding_challenge.Arrays
 {
     public class DeleteElement
     {
-    }
+        public static int[] DeleteElementInArray(int[] arr)
+        {
+
+            int i, j, k;
+
+            int arrLength = arr.Length;
+
+
+
+            for (i = 0; i < arr.Length; i++)
+            {
+
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    //if any duplicate record
+                    if (arr[i] == arr[j])
+                    {
+                        //delete duplicate
+                        for (k = j; k < arrLength - 1; k++)
+                        {
+                            arr[k] = arr[k + 1];
+                            //decrease size after removing duplicate
+                            arrLength--;
+                            //if shifting of element occurs then don't increment
+                            j--;
+                        }
+                    }
+                }
+
+                //print array after deleting duplicate elements
+                string res = "";
+                for (i = 0; i < arr.Length; i++)
+                {
+                    res += arr[i] + ",";
+                }
+
+                string trimmedStr = res.TrimEnd(',');
+
+                //return trimmedStr;
+
+                int[] arrConv = Array.ConvertAll<string, int>(trimmedStr.Split(','), Convert.ToInt32);
+
+                return arrConv;
+
+
+
+
+
+            }
+        }
 }
