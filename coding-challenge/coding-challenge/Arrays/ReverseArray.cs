f3b858181c7e8ab4080fbf85e584846c8ce46320
Nonso <justcoolk@yahoo.com>
2023-02-19 23:37:25 +0100

    implement reverse array

--- coding-challenge/coding-challenge/Arrays/ReverseArray.cs
+++ coding-challenge/coding-challenge/Arrays/ReverseArray.cs
@@ -8,5 +8,19 @@ namespace coding_challenge.Arrays
 {
     public class ReverseArray
     {
+        public static bool ReverseArrayElements(params int[] x)
+        {
+
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            foreach (int i in x)
+            {
+                if (d.ContainsKey(i))
+                    return true;
+                else d.Add(i, 1);
+            }
+
+            return false;
+
+        }
     }
 }
