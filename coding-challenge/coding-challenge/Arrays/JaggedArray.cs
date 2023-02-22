2d8f1de26473ff92f4db3043bbe34c9bee190bfc
Nonso <justcoolk@yahoo.com>
2023-02-22 07:21:01 +0100

    create jagged array

--- coding-challenge/coding-challenge/Arrays/JaggedArray.cs
+++ coding-challenge/coding-challenge/Arrays/JaggedArray.cs
@@ -8,5 +8,15 @@ namespace coding_challenge.Arrays
 {
     public class JaggedArray
     {
+        public void CreateJaggedArray()
+        {
+
+            int[][] a = new int[4][]; //4 rows, columns unknown
+            a[0] = new int[3] { 27, 31, 40 };
+            a[1] = new int[4] { 50, 30, 20, 67 };
+            a[2] = new int[2] { 20, 90 };
+            a[3] = new int[5] { 37, 9, 10,44, 80 };
+
+        }
     }
 }
