3a68c0865aaa91a13733c109959a0191837ccd0a
Nonso <justcoolk@yahoo.com>
2023-02-28 23:01:12 +0100

    implement move zero algo challenge

--- coding-challenge/coding-challenge/Arrays/MoveZeros.cs
+++ coding-challenge/coding-challenge/Arrays/MoveZeros.cs
@@ -8,5 +8,13 @@ namespace coding_challenge.Arrays
 {
     public class MoveZeros
     {
+        public static void Move(params int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            
+        }
     }
 }
