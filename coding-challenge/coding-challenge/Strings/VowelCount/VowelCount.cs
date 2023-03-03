f0b44d3047816349a650c9781211374a004c692b
Nonso <justcoolk@yahoo.com>
2023-03-03 23:48:03 +0100

    vowel algo challenge

--- coding-challenge/coding-challenge/Strings/VowelCount/VowelCount.cs
+++ coding-challenge/coding-challenge/Strings/VowelCount/VowelCount.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace coding_challenge.Strings.VowelCount
 {
     public class VowelCount
     {
+        public static int CountVowelsInAStirng(string str)
+        {
+            var regex = new Regex("a|e|i|o|u");
+            return regex.Matches(str).Count;
+        }
     }
 }
