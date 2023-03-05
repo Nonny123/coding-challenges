9af57d064f06342ef862ab5004ccf66b16bde9c7
Nonso <justcoolk@yahoo.com>
2023-03-05 14:11:40 +0100

    implement unique characters return list

--- coding-challenge/coding-challenge/Strings/UniqueCharacters/UniqueCharacters.cs
+++ coding-challenge/coding-challenge/Strings/UniqueCharacters/UniqueCharacters.cs
@@ -8,5 +8,18 @@ namespace coding_challenge.Strings.UniqueCharacters
 {
     public class UniqueCharacters
     {
+        public static List<char> UniqueList(string s)
+        {
+            List<char> chars = new List<char>();
+            foreach(char c in s)
+            {
+                if(!chars.Contains(c))
+                {
+                    chars.Add(c);
+                }
+            }
+
+            return chars;
+        }
     }
 }
