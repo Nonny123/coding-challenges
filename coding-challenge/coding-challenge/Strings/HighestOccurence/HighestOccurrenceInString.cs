c6a37a7010d811e43fa8f6662e448e2189abf697
Nonso <justcoolk@yahoo.com>
2023-03-10 23:06:49 +0100

    highest occurence algo

--- coding-challenge/coding-challenge/Strings/HighestOccurence/HighestOccurrenceInString.cs
+++ coding-challenge/coding-challenge/Strings/HighestOccurence/HighestOccurrenceInString.cs
@@ -12,7 +12,19 @@ namespace coding_challenge.Strings.HighestOccurence
         {
             int CHAR_SIZE = 256;
             int[] arrCount = new int[CHAR_SIZE];
-            int strLength = s.Length
+            int strLength = s.Length;
+            for(int i = 0; i < strLength; i++)
+            {
+                arrCount[s[i]]++;
+            }
+            int maxVal = -1;
+            char res = '';
+
+            for (int i = 0; i < strLength; i++)
+            {
+                arrCount[s[i]]++;
+            }
+
         }
     }
 }
