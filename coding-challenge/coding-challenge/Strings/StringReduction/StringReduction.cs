19cc155a418ab5d9b089dcd30e452454fcb9d5c5
Nonso <justcoolk@yahoo.com>
2023-03-13 22:01:57 +0100

    string reduction challenge

--- coding-challenge/coding-challenge/Strings/StringReduction/StringReduction.cs
+++ coding-challenge/coding-challenge/Strings/StringReduction/StringReduction.cs
@@ -8,5 +8,20 @@ namespace coding_challenge.Strings.StringReduction
 {
     public class StringReduction
     {
+        public static string StringReduction(string str)
+        {
+            int a = 0; int b = 0; int c = 0;
+            
+            foreach(char cc in str)
+            {
+                if (cc == 'a') a++;
+                if (cc == 'b') b++;
+                if (cc == 'c') c++;
+            }
+
+            int max = 0;
+
+            max = Math.Max(max, Math.Abs(a - b));
+        }
     }
 }
