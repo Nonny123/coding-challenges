382235cba57be9446e1ca281879b43e80cda2afd
Nonso <justcoolk@yahoo.com>
2023-03-17 22:46:00 +0100

    remove brackets in string solution

--- coding-challenge/coding-challenge/Strings/RemoveBrackets/RemoveBrackets.cs
+++ coding-challenge/coding-challenge/Strings/RemoveBrackets/RemoveBrackets.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace coding_challenge.Strings.RemoveBrackets
 {
-    class RemoveBrackets
+    public class RemoveBrackets
     {
+        public static string RemoveBracketsInString(string str)
+        {
+            while(str.Contains("()"))
+            {
+                str = str.Replace("()", "");
+            }
+            str = str.Length.ToString();
+            return str;
+        }
     }
 }
