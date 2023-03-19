65469daafbde7e4e1413edc6385d2b712c4e67f8
Nonso <justcoolk@yahoo.com>
2023-03-19 18:51:00 +0100

    fibonnaci checker solution

--- coding-challenge/coding-challenge/Strings/FibonnaciChecker/FibonnaciChecker.cs
+++ coding-challenge/coding-challenge/Strings/FibonnaciChecker/FibonnaciChecker.cs
@@ -8,5 +8,28 @@ namespace coding_challenge.Strings.FibonnaciChecker
 {
     public class FibonnaciChecker
     {
+        public static string FibonnaciCheck(int num)
+        {
+            if(num == 0 || num == 1)
+            {
+                return "yes";
+            }
+            int sum1 = 0;
+            int sum2 = 1;
+            int sumAxlr = 0;
+
+            while(sum2 <= num)
+            {
+                sumAxlr = sum1 + sum2;
+                sum1 = sum2;
+                sum2 = sumAxlr;
+                if(sum2 == num)
+                {
+                    return "yes";
+                }
+            }
+
+            return "no";
+        }
     }
 }
