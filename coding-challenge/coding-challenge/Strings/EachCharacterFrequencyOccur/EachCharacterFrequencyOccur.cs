f9af7e4d359b02549b1d30e5fb01e863970a565f
Nonso <justcoolk@yahoo.com>
2023-03-21 23:32:46 +0100

    Character frequency challenge

--- coding-challenge/coding-challenge/Strings/EachCharacterFrequencyOccur/EachCharacterFrequencyOccur.cs
+++ coding-challenge/coding-challenge/Strings/EachCharacterFrequencyOccur/EachCharacterFrequencyOccur.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace coding_challenge.Strings.EachCharacterFrequencyOccur
 {
     public class EachCharacterFrequencyOccur
     {
+        public static List<char> CharacterFrequency(string s)
+        {
+            List<char> chars = new List<char>();
+            foreach (char c in s)
+            {
+                if (!chars.Contains(c))
+                {
+                    chars.Add(c);
+                }
+            }
+
+            return chars;
+        }
     }
 }
