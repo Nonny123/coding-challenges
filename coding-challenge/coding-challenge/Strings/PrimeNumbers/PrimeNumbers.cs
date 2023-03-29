1a379de0b928802f4c9e064e20fa5af88ba234f1
Nonso <justcoolk@yahoo.com>
2023-03-29 22:59:11 +0100

    fix prime numbers challenge

--- coding-challenge/coding-challenge/Strings/PrimeNumbers/PrimeNumbers.cs
+++ coding-challenge/coding-challenge/Strings/PrimeNumbers/PrimeNumbers.cs
@@ -8,5 +8,18 @@ namespace coding_challenge.Strings.PrimeNumbers
 {
     public class PrimeNumbers
     {
+        public static string Primes(int num)
+        {
+            if (num < 1) return "false";
+
+            if (num > (int)UInt16.MaxValue + 1) return "false";
+
+            for(int i = 2; i < num/2 + 1; i++)
+            {
+                return "false";
+            }
+
+            return "true";
+        }
     }
 }
