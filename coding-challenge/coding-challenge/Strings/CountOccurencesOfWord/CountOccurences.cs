87b21dada968df9a72dd00ab56c5d0fd2bc576f3
Nonso <justcoolk@yahoo.com>
2023-04-02 19:18:43 +0100

    implement count word occurences in sentence

--- coding-challenge/coding-challenge/Strings/CountOccurencesOfWord/CountOccurences.cs
+++ coding-challenge/coding-challenge/Strings/CountOccurencesOfWord/CountOccurences.cs
@@ -8,5 +8,16 @@ namespace coding_challenge.Strings.CountOccurencesOfWord
 {
     public class CountOccurences
     {
+        public static int CountOccurencesInSentence(string str, string word)
+        {
+            string[] a = str.Split(' ');
+            int count = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (word.Equals(a[i])) count++;
+            }
+
+            return count;
+        }
     }
 }
