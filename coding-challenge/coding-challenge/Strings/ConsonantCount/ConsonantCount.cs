e21ba3c4175b37aefbfe5f21896993f23764809e
Nonso <justcoolk@yahoo.com>
2023-04-21 19:00:22 +0100

    consonant count algo challenge

---
+++ coding-challenge/coding-challenge/Strings/ConsonantCount/ConsonantCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge.Strings.ConsonantCount
+{
+    public class ConsonantCount
+    {
+
+    }
+}
