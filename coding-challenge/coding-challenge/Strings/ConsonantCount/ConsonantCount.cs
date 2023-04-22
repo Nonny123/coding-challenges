983048e0b957e7f9c05f8d1915bf2d9d44f30013
Nonso <justcoolk@yahoo.com>
2023-04-22 20:51:03 +0100

    Implement CountConsonant in String algo

--- coding-challenge/coding-challenge/Strings/ConsonantCount/ConsonantCount.cs
+++ coding-challenge/coding-challenge/Strings/ConsonantCount/ConsonantCount.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace coding_challenge.Strings.ConsonantCount
 {
     public class ConsonantCount
     {
-
+        public static int CountConsonant(string str)
+        {
+            Regex rgx = new Regex(@"[^a-zA-Z]|[aeiou]", RegexOptions.IgnoreCase);
+            int num = rgx.Replace(str, "").Length;
+            return num;
+        }
     }
 }
