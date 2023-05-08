8fc3f5a5acc6bba1fa89f21082d1573d28fb31fc
Nonso <justcoolk@yahoo.com>
2023-05-08 21:14:33 +0100

    bird count challenge

---
+++ coding-challenge/coding-challenge/ExercismChallenges/Arrays/BirdWatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge.ExercismChallenges.Arrays
+{
+    class BirdCount
+    {
+        private int[] birdsPerDay;
+
+        public BirdCount(int[] birdsPerDay)
+        {
+            this.birdsPerDay = birdsPerDay;
+        }
+
+        public static int[] LastWeek()
+        {
+            int[] lastWeek = { 0, 2, 5, 3, 7, 8, 4 };
+            return lastWeek;
+        }
+
+        public int Today()
+        {
+            throw new NotImplementedException("Please implement the BirdCount.Today() method");
+        }
+
+        public void IncrementTodaysCount()
+        {
+            throw new NotImplementedException("Please implement the BirdCount.IncrementTodaysCount() method");
+        }
+
+        public bool HasDayWithoutBirds()
+        {
+            throw new NotImplementedException("Please implement the BirdCount.HasDayWithoutBirds() method");
+        }
+
+        public int CountForFirstDays(int numberOfDays)
+        {
+            throw new NotImplementedException("Please implement the BirdCount.CountForFirstDays() method");
+        }
+
+        public int BusyDays()
+        {
+            throw new NotImplementedException("Please implement the BirdCount.BusyDays() method");
+        }
+    }
+
+}
