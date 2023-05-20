c2200713babb432925134f9318acea5c59349a4e
Nonso <justcoolk@yahoo.com>
2023-05-20 17:55:44 +0100

    implement appointment schedule

--- coding-challenge/coding-challenge/ExercismChallenges/DateManipulation/AppointmentSchedule.cs
+++ coding-challenge/coding-challenge/ExercismChallenges/DateManipulation/AppointmentSchedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,27 +11,46 @@ namespace coding_challenge.ExercismChallenges.DateManipulation
     {
         public static DateTime Schedule(string appointmentDateDescription)
         {
-            throw new NotImplementedException("Please implement the (static) Appointment.Schedule() method");
+            DateTime result;
+            DateTime.TryParseExact(appointmentDateDescription, "M/d/yyyy HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+
+            return result;
+
         }
 
         public static bool HasPassed(DateTime appointmentDate)
         {
-            throw new NotImplementedException("Please implement the (static) Appointment.HasPassed() method");
+            return appointmentDate < Appointment.Schedule("7/25/2019 13:45:00") ? true : false;
         }
 
         public static bool IsAfternoonAppointment(DateTime appointmentDate)
         {
-            throw new NotImplementedException("Please implement the (static) Appointment.IsAfternoonAppointment() method");
+            return appointmentDate.Hour >= 12 && appointmentDate.Hour < 18 ? true : false;
         }
 
         public static string Description(DateTime appointmentDate)
         {
-            throw new NotImplementedException("Please implement the (static) Appointment.Description() method");
+            string check = string.Empty;
+           
+            switch (appointmentDate.Hour)
+            {
+                case var hour when hour >= 0 && hour < 12:
+                    check = "AM";
+                    break;
+                case var hour when hour >= 12 && hour < 24:
+                    check = "PM";
+                    break;
+                default:
+                    check = "Invalid hour";
+                    break;
+            }
+
+            return $"You have an appointment on {appointmentDate.ToString("M/d/yyyy")} {appointmentDate.ToString("hh:mm:ss tt")}.";
         }
 
         public static DateTime AnniversaryDate()
         {
-            throw new NotImplementedException("Please implement the (static) Appointment.AnniversaryDate() method");
+            return new DateTime(2019, 9, 15, 0, 0, 0);
         }
     }
 }
