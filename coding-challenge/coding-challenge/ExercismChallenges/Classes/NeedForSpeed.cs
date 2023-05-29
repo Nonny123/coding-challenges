da100ac64984ed26481e3b622aacf0079bfe56cc
Nonso <justcoolk@yahoo.com>
2023-05-29 10:56:24 +0100

    Needforspeed challenge

---
+++ coding-challenge/coding-challenge/ExercismChallenges/Classes/NeedForSpeed.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge.ExercismChallenges.Classes
+{
+    class NeedForSpeedRemoteControlCar
+    {
+        // TODO: define the constructor for the 'RemoteControlCar' class
+
+        public bool BatteryDrained()
+        {
+            throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+        }
+
+        public int DistanceDriven()
+        {
+            throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
+        }
+
+        public void Drive()
+        {
+            throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        }
+
+        public static RemoteControlCar Nitro()
+        {
+            throw new NotImplementedException("Please implement the (static) RemoteControlCar.Nitro() method");
+        }
+    }
+
+    class RaceTrack
+    {
+        // TODO: define the constructor for the 'RaceTrack' class
+
+        public bool TryFinishTrack(RemoteControlCar car)
+        {
+            throw new NotImplementedException("Please implement the RaceTrack.TryFinishTrack() method");
+        }
+    }
+
+}
